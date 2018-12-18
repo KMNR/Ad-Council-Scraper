f5923f73f67a77ae0096928f35bbd43a07f34c2e
Brett Heinkel <bheinks@gmail.com>
2018-12-17 22:57:56 -0600

    Add Campaign class, Refactor to use the "Browse Campaigns" page instead of search, clear directory before downloading, other misc changes

--- scrape.py
+++ scrape.py
@@ -4,17 +4,30 @@ import requests
 import os
 
 from bs4 import BeautifulSoup
+from shutil import rmtree
 from sys import argv
 
-# search results of all english radio programming from the past 3 months
-SEARCH_URL = "https://www.psacentral.org/search?s=&q=&t=radio&l=english&rd=past-3-months&ma=all&ms=all"
+# list of campaigns
+BROWSE_URL = "https://www.psacentral.org/browse-campaigns"
 
 # base URL for requesting campaign JSON objects
-API_URL = "https://www.psacentral.org/api/group?id={}"
+API_URL = "https://www.psacentral.org/api/group?id="
+
+# list of campaigns that we don't want to download
+BLACKLIST = [
+    "https://www.psacentral.org/campaign/emergency-preparedness-nyc"
+]
+
+
+class Campaign:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
 
 
 class Asset:
-    def __init__(self, url, title, length, format):
+    def __init__(self, campaign, url, title, length, format):
+        self.campaign = campaign
         self.url = url
         self.title = title
         self.length = length
@@ -24,59 +37,89 @@ class Asset:
 
 
 def main(download_location):
-    campaign_ids = get_campaign_ids(SEARCH_URL)
-    assets = get_assets(campaign_ids)
-    download_assets(assets, download_location)
+    campaigns = get_campaigns(BROWSE_URL)
+    assets = get_assets(campaigns)
+    clear_dir(download_location) # clear download location
+    download_assets(assets, download_location) # download assets to download location
 
 
-# get unique ID for each campaign
-def get_campaign_ids(url):
-    campaign_ids = []
-
+# fetch campaign information and return list of Campaign objects
+def get_campaigns(url):
+    # get HTML of campaign list and parse
     r = requests.get(url)
     soup = BeautifulSoup(r.text, "html.parser")
 
-    for campaign in soup.find_all("div", class_="GroupPromo"):
-        campaign_ids.append(campaign["data-campaign-asset-group-id"])
+    # find all campaign links
+    campaign_links = []
+    for tag in soup.find_all("div", class_="CampaignPromo-media"):
+        link = tag.a["href"]
 
-    return campaign_ids
+        # prevent duplicates and blacklisted campaigns from being added
+        if link not in campaign_links and link not in BLACKLIST:
+            campaign_links.append(tag.a["href"])
+
+    campaigns = []
+    for link in campaign_links:
+        r = requests.get(link)
+        soup = BeautifulSoup(r.text, "html.parser")
+
+        # find all campaign IDs and names
+        for tag in soup.find_all("div", class_="GroupPromo"):
+            # only radio campaigns
+            if tag["data-type"] == "Radio":
+                campaign = Campaign(tag["data-campaign-asset-group-id"], tag["data-campaign-name"])
+                campaigns.append(campaign)
+
+    return campaigns
 
 
 # fetch asset information and return list of Asset objects
-def get_assets(campaign_ids):
+def get_assets(campaigns):
     assets = []
 
-    for id in campaign_ids:
+    for campaign in campaigns:
         # get JSON object
-        r = requests.get(API_URL.format(id))
+        r = requests.get(API_URL + campaign.id)
 
         # loop over assets
         for a in r.json():
             # only english assets
             if a["language"] == "English":
-                # dumb additional condition to prevent downloading assets for US territories
+                # prevent downloading market specific assets if market area is not specified or not MO
                 if "Market Specific" in a["title"] and (not a["marketArea"] or "MO" not in a["marketArea"]):
                     continue
 
-                # create asset object, striping leading colon from length and making format lowercase
-                assets.append(Asset(a["sourceUrl"], a["title"], a["length"][1:], a["fileFormat"].lower()))
+                # create asset object, stripping leading colon from length and making format lowercase
+                asset = Asset(campaign, a["sourceUrl"], a["title"], a["length"][1:], a["fileFormat"].lower())
+                assets.append(asset)
 
     return assets
+
+
+# clear all files and folders out of the specified path (USE WITH CARE)
+def clear_dir(path):
+    for filename in os.listdir(path):
+        file_path = os.path.join(path, filename)
+
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+        elif os.path.isdir(file_path):
+            rmtree(file_path)
 
 
 # download assets to specified location
 def download_assets(assets, download_location):
     for asset in assets:
-        path = os.path.join(download_location, asset.title)
+        path = os.path.join(download_location, asset.campaign.name)
 
         # if the directory doesn't exist, create it
         if not os.path.exists(path):
             os.makedirs(path)
 
-        # get asset data
+        # get asset audio
         r = requests.get(asset.url)
 
-        # write MP3 data to file
+        # write audio data to file
         with open(os.path.join(path, asset.filename), "wb") as f:
             f.write(r.content)
 
